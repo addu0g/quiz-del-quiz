837ceddb4f107b079a14ca037ac3c1177fe41670
addu0g <addu0g@live.com.mx>
2015-06-17 06:08:50 -0500

    Se agrega busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,48 @@
 ﻿var models = require('../models/models.js');
 
-exports.question = function(req, res){
-	models.Quiz.findAll().success(function(quiz){
-		res.render('quizes/question', {pregunta:quiz[0].pregunta});
+exports.load = function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			}else{
+				next(new Error("No existe quizId="+quizId));
+			}
+		}
+	).catch(function(error){
+		next(error);
+	});
+};
+
+exports.index = function(req, res){
+	if(req.query.search === undefined){
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index.ejs', {quizes:quizes});
+		});
+	}
+	else{
+		var regular =new RegExp(' ', 'g'); 
+		var patron = ('%'+req.query.search+'%').replace(regular, '%');
+		models.Quiz.findAll({where:["pregunta like ?", patron]}).then(function(quizes){
+		res.render('quizes/index.ejs', {quizes:quizes});
+		});		
+	}
+};
+
+exports.show = function(req, res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', {quiz: req.quiz});
 	})	
 };
 
 exports.answer = function(req, res){
-	models.Quiz.findAll().success(function(quiz){
-		if(req.query.respuesta.toLowerCase()=== quiz[0].respuesta.toLowerCase()){
-			res.render('quizes/answer', { respuesta:'Correcto' });
-		}else{
-			res.render('quizes/answer', { respuesta:'Incorrecto' });
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		var resultado = 'Incorrecto';
+		if(req.query.respuesta.toLowerCase()=== req.quiz.respuesta.toLowerCase()){
+			resultado='Correcto';
 		}
+		res.render('quizes/answer', {quiz:req.quiz,  respuesta:resultado });
+				
 	});
 };
